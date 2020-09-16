11d5fe54b0c144072747c06a83cc3a60ae8d77f2
FrancisKo <kiragufrancis97@gmail.com>
2020-09-16 17:21:24 +0300

    Changed package.json

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+let app = express()
+let port = process.env.PORT || 3000
+let publicFolder = path.join(__dirname, './public')
+app.use(express.static(publicFolder))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(publicFolder, 'index.html'))
+})
+app.listen(port, () => {
+    console.log(
+     'Node app running...'   
+    )
+})
---
+++ src/Main.js
@@ -0,0 +1,106 @@
+import React, { useEffect } from 'react';
+const Main = (props) => {
+    const filters = ['all', 'active', 'done']
+    const [todos, setTodos] = React.useState([
+        { title: 'Learn Jest Testing', status: 'active' },
+        { title: 'Learn React Native', status: 'active' },
+        { title: 'Learn Django', status: 'done' }
+    ])
+    const [filter, setFilter] = React.useState(filters[0])
+    const [filteredTodos, setFiltered] = React.useState([])
+    const [input , setInput ] = React.useState('')
+
+    const filterTodos = (item) => {
+        setFilter(item)
+        switch (item) {
+            case 'all': setFiltered(() => {
+                return todos
+            })
+                break;
+            case 'done': setFiltered(() => {
+                return todos.filter(todo => todo.status === 'done')
+            })
+                break;
+            case 'active': setFiltered(() => {
+                return todos.filter(todo => todo.status === 'active')
+            })
+                break;
+            default:
+                return todos
+        }
+    }
+    const removeTodo = (mytodo) => {
+        setTodos([...todos.filter(todo => todo.title !== mytodo)])
+    }
+
+    const toggleTodo = (mytodo) => {
+        setTodos([...todos.map(todo => {
+            if(todo.title === mytodo){
+                return {
+                    title: mytodo, status : todo.status === 'active' ? 'done' : 'active'
+                }
+            }else{
+                return todo
+            }
+        })])
+    }
+
+    const addTodo = e => {
+        if(e.key === 'Enter' && input !== ''){ 
+            setTodos([...todos, { title:input, status:'active'} ])
+            setInput('')
+         }      
+    }
+    useEffect(() => {
+        filterTodos(filter)
+    }, [filter, todos])
+    return (
+        <div className="container">
+            <div className="section">
+                <div className="center">
+                    <h5 className="teal-text">TODO APP</h5><hr></hr>
+                </div>
+                <div className='row'>
+                    <div className="col l4 m4 s12">
+                        <ul className="collection">
+                            {
+                                filters.map(filt =>
+                                    <li key={filt} className="btn waves-effect" onClick={() => filterTodos(filt)} className={`collection-item ${filt === filter ? 'white-text purple' : ''}`}>
+                                        {filt.toString().toUpperCase()}
+                                    </li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                        <div className="input-field  col l8 m8 s12">
+                            <label className="active" htmlFor="todo">Enter todo...</label>
+                            <input 
+                            onChange={(e) => setInput(e.target.value)}
+                            onKeyUp = {addTodo}
+                            value={input}
+                            type="text" name="todo" id="todo" /><br></br>
+                        </div>
+                </div>
+                <div className="center">
+                    <h5 className="teal-text">{`${filter.toString().toUpperCase()} TODOS`}</h5>
+                </div>
+                <ul className="collection">
+                    {
+                        filteredTodos.length > 0 ? filteredTodos.map(todo => {
+                            return (
+                                <li style={todo.status ==='done' ? {textDecorationLine:'line-through'} : {}} className="collection-item" key={todo.title}>{`${todo.title} -- ${todo.status}`}
+                                    <button onClick={ () => removeTodo(todo.title) } className="red white-text right" style={{borderRadius:'3px', border:'green', padding:'5px'}}> Delete </button>
+                                    <button onClick={ () => toggleTodo(todo.title)  } className="green white-text right bt" style={{borderRadius:'3px', border:'green', padding:'5px'}}> Toggle </button>
+                                </li>
+                            )
+                        })
+                            :
+                            <li className="collection-item">No todos available yet!</li>
+                    }
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
