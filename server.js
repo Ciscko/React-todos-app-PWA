521e884025e69aad93b7e297a532208b552bec82
FrancisKo <kiragufrancis97@gmail.com>
2020-09-16 17:37:02 +0300

    Told the app, the statics are now in the build folder after app runs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 let app = express()
 let port = process.env.PORT || 3000
-let publicFolder = path.join(__dirname, './public')
+let publicFolder = path.join(__dirname,'.','build')
 app.use(express.static(publicFolder))
 app.get("*", (req, res) => {
     res.sendFile(path.join(publicFolder, 'index.html'))
